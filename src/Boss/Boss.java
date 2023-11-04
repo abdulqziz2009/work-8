eeaf1c4e7a9190e7217901ed8d63477ab771dc3d
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 13:56:03 +0600

    Initial commit

---
+++ src/Boss/Boss.java
@@ -0,0 +1,2 @@
+package Boss;public class Boss {
+}
---
+++ src/Hero/Hero.java
@@ -0,0 +1,26 @@
+package Hero;
+
+import Boss.Boss;
+
+public abstract class Hero {
+    public Hero(int i, int i1, String rockShield) {
+    }
+
+    public abstract void applySuperAbility(Boss boss);
+
+    protected boolean isAlive() {
+        boolean b = false;
+        return b;
+    }
+
+    public static class hero {
+        public class hiro {
+
+            public void revive() {
+            }
+
+            public void isAlive() {
+            }
+        }
+    }
+}
